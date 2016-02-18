e5a2d69a8ee164e135aa2a7103bcd048c5f9f73e
yahsan2 <yahsan2@gmail.com>
2016-02-18 17:24:26 +1000

    jQuery import

--- prototype/src/javascripts/app.js
+++ prototype/src/javascripts/app.js
@@ -1,19 +1,34 @@
 import './asyncModules'
 import exclaimify from './exclaimify'
 
-const button = document.getElementById('button')
+const $ = require('jquery')
 
-const alertAsyncMessage = function() {
-  // CommonJS async syntax webpack magic
-  require.ensure([], function() {
-    const message = require("./asyncMessage")
-    alert(exclaimify(message))
-  })
+class App {
+  constructor(el) {
+    this.el = el
+    this.$el = $(el)
+    this.initialize()
+  }
+
+  initialize() {
+    console.log(`
+      asset references like this one:
+      images/gulp.png
+      get updated in js too!
+    `)
+
+    const button = document.getElementById('button')
+    button.addEventListener('click', function() {
+      // CommonJS async syntax webpack magic
+      require.ensure([], function() {
+        const message = require("./asyncMessage")
+        alert(exclaimify(message))
+      })
+    })
+  }
 }
 
-console.log(`
-  asset references like this one:
-    images/gulp.png
-  get updated in js too!`)
 
-button.addEventListener('click', alertAsyncMessage)
+$(function(){
+  const app = new App();
+})
