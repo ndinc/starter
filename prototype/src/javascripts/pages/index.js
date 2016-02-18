fa748b1a4c983fde0781295bc932a2e449c3ffe1
yahsan2 <yahsan2@gmail.com>
2016-02-18 19:22:59 +1000

    Add imagesLoaded.js

--- prototype/src/javascripts/pages/index.js
+++ prototype/src/javascripts/pages/index.js
@@ -1,14 +1,28 @@
 // ページごとに読み込まれるファイル
 // Must use "const" or "var"
+const imagesLoaded = require('imagesLoaded')
+const $ = require('jquery')
 
 export default class Index {
   constructor(id) {
     this.id = id
-    this.initialize()
-    // code
+    require.ensure(['jquery','imagesloaded'], ()=> {
+      this.initialize()
+    });
   }
 
   initialize() {
+    this.sample_imagesloaded()
+    this.sample_message()
+  }
+
+  sample_imagesloaded(){
+    imagesLoaded('#main', ()=>{
+      console.log('loaded');
+    });
+  }
+
+  sample_message(){
     const message = require('../async/message.js')
     console.log(message(`${this.id}.js loaded`));
   }
