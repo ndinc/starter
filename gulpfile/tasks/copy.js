ebf607f9666c2a1ba8d40aaf5f0ae55d646b41f6
yahsan2 <yahsan2@gmail.com>
2016-02-18 16:20:15 +1000

    Add build files

---
+++ gulpfile/tasks/copy.js
@@ -0,0 +1,21 @@
+var config      = require('../config')
+
+var browserSync = require('browser-sync')
+var changed     = require('gulp-changed')
+var gulp        = require('gulp')
+var path        = require('path')
+
+var paths = {
+  src: path.join(config.root.dest, '/**'),
+  dest: config.root.build
+}
+
+var copyTask = function() {
+  return gulp.src(paths.src)
+    .pipe(changed(paths.dest)) // Ignore unchanged files
+    .pipe(gulp.dest(paths.dest))
+    .pipe(browserSync.stream())
+}
+
+gulp.task('copy', copyTask)
+module.exports = copyTask
