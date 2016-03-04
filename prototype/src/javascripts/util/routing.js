82d1cb66f6cb0ae2835fd07967a178d41b983fe2
yahsan2 <yahsan2@gmail.com>
2016-03-04 11:34:23 +0900

    Add sample layout

--- prototype/src/javascripts/util/routing.js
+++ prototype/src/javascripts/util/routing.js
@@ -1,6 +1,8 @@
 export default () => {
   const body = document.getElementsByTagName('body')
   const pageId = body[0].getAttribute('data-page-id');
-  const Module = require(`../pages/${pageId}`)
-  new Module(pageId)
+  if (pageId) {
+    const Module = require(`../pages/${pageId}`)
+    new Module(pageId)
+  }
 }
