80c46bd2ea77ba3f03a40b2ac4df534ca73b2c16
yahsan2 <yahsan2@gmail.com>
2016-03-09 20:46:28 +0900

    Modify tags in wordpress

--- gulpfile/extra/wp/starter-theme/templates/pages/home.php
+++ gulpfile/extra/wp/starter-theme/templates/pages/home.php
@@ -2,7 +2,7 @@
   <header class="s-home_header page-header">
     <div class="row">
       <div class="small-12 medium-12 large-12 columns">
-        <h1 class="e-page_title">Starter Posts (Example)</h1>
+        <h2 class="e-page_title">Starter Posts (Example)</h2>
       </div>
     </div>
   </header>
@@ -10,7 +10,7 @@
     <div class="row">
       <div class="small-12 medium-8 large-8 columns">
         <section class="content-section">
-          <h2 class="e-section-title">Recommend Posts</h2>
+          <h3 class="e-section-title">Recommend Posts</h3>
           <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-2">
             <?php $wp_post_query = get_post_query('post', 10); ?>
             <?php while ($wp_post_query->have_posts()) : $wp_post_query->the_post(); ?>
@@ -19,7 +19,7 @@
           </ul>
         </section>
         <section class="content-section">
-          <h2 class="e-section-title">Recently Posts</h2>
+          <h3 class="e-section-title">Recently Posts</h3>
           <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-2">
             <?php $wp_post_query = get_post_query('post', 10); ?>
             <?php while ($wp_post_query->have_posts()) : $wp_post_query->the_post(); ?>
--- gulpfile/extra/wp/starter-theme/templates/sections/header.php
+++ gulpfile/extra/wp/starter-theme/templates/sections/header.php
@@ -3,7 +3,11 @@
       <div class="row">
         <div class="small-12 medium-12 large-12 columns">
           <section class="s-header_title content-section">
+            <?php if (is_home()): ?>
             <h1 class="e-header_title">Starter</h1>
+            <?php else: ?>
+            <p class="e-header_title">Starter</p>
+            <?php endif ?>
             <p class="e-header_description">This is starter template created by nD inc.</p>
           </section>
           <section class="content-section m-header_nav" js-hamburger>
