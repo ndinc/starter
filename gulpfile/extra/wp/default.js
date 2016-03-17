9b552e356df1a772417c55866bbdfd8d2039a28c
yahsan2 <yahsan2@gmail.com>
2016-03-17 20:50:50 +0900

    Fix wp default task

--- gulpfile/extra/wp/default.js
+++ gulpfile/extra/wp/default.js
@@ -4,7 +4,7 @@ var getEnabledTasks = require('../../lib/getEnabledTasks')
 
 var defaultTask = function(cb) {
   var tasks = getEnabledTasks('watch')
-  gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, 'static', 'wp:watch', cb)
+  gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, 'static', 'wp:data', 'wp:copy', 'wp:watch', cb)
 }
 
 gulp.task('wp', defaultTask)
