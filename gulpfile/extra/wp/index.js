ba32ce5dfb4a822a492f3020bea3e882c03bfbbf
Ryosuke Miyamoto <miyamoto@ndinc.jp>
2016-03-28 01:01:50 +0900

    Update wp:setup task

--- gulpfile/extra/wp/index.js
+++ gulpfile/extra/wp/index.js
@@ -1,21 +1,37 @@
+var options       = require('./options')
 var gulp         = require('gulp')
 var gulpSequence = require('gulp-sequence')
 var exec = require('child_process').exec;
+var fs = require('fs')
 
 var wordpressTask = function(cb) {
-  exec('source ~/.bash_profile', function(err, stdout, stderr){
-    gulpSequence(
-      'wp:download',
-      'wp:config',
-      'wp:db:create',
-      'wp:directory',
-      'wp:install',
-      'wp:plugin',
-      'wp:theme',
-      'wp:option',
-      'wp:production',
-    cb);
-  });
+  try {
+    var wp = fs.readFileSync(options.download.path + '/wp-load.php', 'utf8')
+    if (wp) {
+      gulpSequence(
+        'db:create',
+        'db:import',
+        'wp:config',
+        'wp:option',
+        'wp:production',
+      cb);
+    }
+  } catch (e){
+    if(e.code = 'ENOENT'){
+      gulpSequence(
+        'wp:download',
+        'wp:config',
+        'db:create',
+        'wp:directory',
+        'wp:install',
+        'db:dump',
+        'wp:plugin',
+        'wp:theme',
+        'wp:option',
+        'wp:production',
+      cb);
+    }
+  }
 }
 
 gulp.task('wp:setup', wordpressTask)
