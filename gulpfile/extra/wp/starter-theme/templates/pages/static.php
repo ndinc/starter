3efcd87f71d6b49bca5ae0031c6f6db6e5dfa017
Ryosuke Miyamoto <miyamoto@ndinc.jp>
2016-04-06 15:52:10 +0900

    Add static.php

---
+++ gulpfile/extra/wp/starter-theme/templates/pages/static.php
@@ -0,0 +1,19 @@
+<section id="main" class="l-static">
+  <header class="content-section s-article-header has-background text-center">
+    <div class="row">
+      <div class="small-12 medium-12 large-12 columns">
+        <h1 class="e-page_title title"><?php the_title() ?></h1>
+      </div>
+    </div>
+  </header>
+  <section class="s-classlist page-section">
+    <div class="row">
+      <div class="small-12 medium-centered medium-8 large-6 columns">
+        <section class="content-section">
+        <section class="content-section">
+          <?php the_content(); ?>
+        </section>
+      </div>
+    </div>
+  </section>
+</section>
